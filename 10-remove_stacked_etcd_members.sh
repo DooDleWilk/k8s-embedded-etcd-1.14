85728de65f0a8743a3d973f9d72f67c630acfe94
DooDleWilk <jwilk.1978@gmail.com>
2019-08-02 01:54:03 -0700

    last delete script

--- 10-remove_stacked_etcd_members.sh
+++ 10-remove_stacked_etcd_members.sh
@@ -1,8 +1,11 @@
 #!/bin/bash
 source nodes.sh
 
-ssh -t root@${ETCD1_IP} /etcdctl.sh member list > bootstrap_config/etcd_members.txt
-awk -F: '{print $1}' bootstrap_config/etcd_members.txt
+ssh -t root@${ETCD1_IP} /etcdctl.sh member remove $(ssh -t root@${ETCD1_IP} /etcdctl.sh member list | grep ${MASTER1_HOST} | awk -F: '{print $1}')
+ssh -t root@${ETCD1_IP} /etcdctl.sh member remove $(ssh -t root@${ETCD1_IP} /etcdctl.sh member list | grep ${MASTER2_HOST} | awk -F: '{print $1}')
+ssh -t root@${ETCD1_IP} /etcdctl.sh member remove $(ssh -t root@${ETCD1_IP} /etcdctl.sh member list | grep ${MASTER3_HOST} | awk -F: '{print $1}')
 
-#ssh -t root@${ETCD1_IP} /etcdctl.sh member remove
+ssh root@${MASTER1_IP} rm /etc/kubernetes/manifests/etcd.yaml
+ssh root@${MASTER2_IP} rm /etc/kubernetes/manifests/etcd.yaml
+ssh root@${MASTER3_IP} rm /etc/kubernetes/manifests/etcd.yaml
 
